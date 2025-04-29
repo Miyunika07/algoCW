4f900376ee1b7e815fd3545478d018448b4225a5
Miyunika07 <cmiyunika@gmail.com>
2025-04-29 12:23:33 +0530

    updates

--- CW - Algorithm/src/Main.java
+++ CW - Algorithm/src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(new File("benchmarks/ladder_9.txt")); // Input file
+        Scanner sc = new Scanner(new File("benchmarks/bridge_1.txt")); // Input file
         int n = sc.nextInt(); // Number of nodes
         Graph graph = new Graph(n);
 
